3224c040b3d68fbc066b080df1fd3d889544fde9
kevinontariotech <95292313+kevinontariotech@users.noreply.github.com>
2023-11-25 13:49:49 -0500

    Add files via upload

---
+++ Distributed Project/Server/ItemFile.java
@@ -0,0 +1,13 @@
+import java.sql.Date;
+
+public class ItemFileImpl implements ItemFile{
+    private String fileName;
+    private Date lastUpdated;
+    private String fileType;
+    private String fileInfo;
+
+    ItemFileImpl
+    public getFileName () {
+
+    }
+}
