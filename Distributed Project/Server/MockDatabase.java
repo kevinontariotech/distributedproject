7ec7a437032e9087846d6fe472b886e1bc25877a
kevinontariotech <95292313+kevinontariotech@users.noreply.github.com>
2023-11-25 17:17:12 -0500

    Update MockDatabase.java

--- Distributed Project/Server/MockDatabase.java
+++ Distributed Project/Server/MockDatabase.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import Server.Account.AccountType;
+
 public class MockDatabase {
     //this class just holds all the information that should be in the database
-    ItemFile items [];
-    Account accounts [];
-    ActionLog logs[];
+    List<ItemFile> items = new ArrayList<ItemFile>();
+    List<Account> accounts = new ArrayList<Account>();
+    List<ActionLog> logs = new ArrayList<ActionLog>();
 
+    MockDatabase() {
+        Account account0 = new Account("User1", "password", "John Doe", AccountType.ADMIN);
+        Account account1 = new Account("User2", "password", "May Smith", AccountType.OWNER);
+        Account account2 = new Account("User3", "password", "Mary Jane");
+        accounts.add(account0);
+    }
 }
