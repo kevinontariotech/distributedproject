d6bffdff7cc270b932f6f05be017d8840f4f1f39
kevinontariotech <95292313+kevinontariotech@users.noreply.github.com>
2023-11-25 16:50:45 -0500

    Update MockDatabase.java

---
+++ Distributed Project/Server/MockDatabase.java
@@ -0,0 +1,7 @@
+public class MockDatabase {
+    //this class just holds all the information that should be in the database
+    ItemFile items [];
+    Account accounts [];
+    ActionLog logs[];
+
+}
